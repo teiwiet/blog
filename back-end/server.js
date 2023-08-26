6fa04742e8fc6e8f75e324c658f48e0481d6836c
teiwiet <omegalmao1.3@gmail.com>
2023-08-26 14:47:14 +0700

    update

--- back-end/server.js
+++ back-end/server.js
@@ -4,3 +4,12 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 require("dotenv").config();
+
+
+const app = express();
+
+app.use("/lmao",(req,res,next)=>{
+    res.write("<h1>lmao</h1>");
+})
+
+app.listen()
