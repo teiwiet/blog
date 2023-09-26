aa686b1841b6fa265a2e41e60f0dd4a08ce0e4ba
teiwiet <omegalmao1.3@gmail.com>
2023-09-26 23:29:27 +0700

    update

--- back-end-fake/model/user.model.js
+++ back-end-fake/model/user.model.js
@@ -34,5 +34,32 @@ const userSchema = mongoose.Schema({
     }
 
 },{Timestamp : true});
-userSchema
+
+userSchema.virtual("password")
+.set(function (password){
+    this._password = password;
+
+    this.salt = this.makeSalt();
+    this.hash_password = this.encryptPassword(password);
+})
+.get(function(){
+    return this._password;
+})
+
+userSchema.methods = {
+    encryptPassword : function(password){
+        if(!password) return '';
+        try{
+            return crypto.createHmac("sha1",this.salt).
+            update(password).digest("hex");
+        }
+        catch(err){
+            return '';j
+        }
+    },
+    makeSalt : function(){
+        return Math.round(new Date().valueOf()*Math.random() + '');
+    }
+}
+
 module.exports = mongoose.model("User",userSchema)
